efc3af9668eef4b3d13c1ea8cdac2b3c72fa3efc
Lingaraj Sankaravelu <lingaraj.s@hotmail.com>
2022-06-07 15:09:01 +0530

    outputting conditional content and adding return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
-
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
+
+
+
 
 const Expenses = (props) => {
     const [filter, setFilter] = useState(2021)
@@ -13,23 +15,15 @@ const Expenses = (props) => {
         console.log(filterSelection)
 
     }
-    return (
-        <div>
-        <Card className="expenses">
+    let filteredList = props.items.filter((expense) => { return (parseInt(expense.date.getFullYear()) === filter)})
 
-                <ExpensesFilter selected={filter} classname="expenses-filter" filterSelected={onFilterSelected}/>
-            {
-                props.items.filter((expense) => { return (parseInt(expense.date.getFullYear()) === filter)}).map((expense) => (
-                <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-                />
-                ))
-            }
+    return (
+        <li>
+        <Card className="expenses">
+            <ExpensesFilter selected={filter} classname="expenses-filter" filterSelected={onFilterSelected}/>
+            <ExpensesList items={filteredList}/>
         </Card>
-        </div>
+        </li>
     );
 }
 
